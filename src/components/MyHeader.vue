6d8469ed5e0e262f36f496ccb6e5f99d01e33e0c
shengli <1172119623@qq.com>
2019-10-26 10:32:42 +0800

    first commit

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <h1>这里也页头</h1>
+    <hr>
+  </header>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped>
+  h1{
+    text-align:center;
+  }
+</style>
